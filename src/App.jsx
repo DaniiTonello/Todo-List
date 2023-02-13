022c56aa41a18d35c0dbc2127f785bfe77a4e601
Daniela Tonello <daniela.a.tonello@gmail.com>
2023-02-13 11:13:27 -0300

    adding tasks list and animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,81 @@
 import { useState } from 'react';
-import './App.css';
+import TaskList from './components/TaskList/TaskList';
+import { AnimatePresence, motion } from 'framer-motion';
 import useEmblaCarousel from 'embla-carousel-react';
 
-function App() {
-  const [count, setCount] = useState(0);
+const TASKS_LISTS = [
+  {
+    title: 'Trip to Paris',
+    color: 'bg-pink-300',
+    tasks: [
+      {
+        title: 'Passport check',
+        completed: true,
+      },
+      {
+        title: 'Hotel reservation',
+        completed: false,
+      },
+    ],
+  },
+  {
+    title: 'My Tasks',
+    color: 'bg-blue-200',
+    tasks: [
+      {
+        title: 'Buy milk',
+        completed: false,
+      },
+      {
+        title: 'Plan weekend outing',
+        completed: false,
+      },
+      {
+        title: 'Publishing Friday blog post',
+        completed: true,
+      },
+      {
+        title: 'Run 3 miles',
+        completed: false,
+      },
+      {
+        title: 'Wash clothes',
+        completed: true,
+      },
+    ],
+  },
+  {
+    title: 'Work',
+    color: 'bg-yellow-300',
+    tasks: [
+      {
+        title: 'Meeting at 9am',
+        completed: false,
+      },
+      {
+        title: 'Update database',
+        completed: true,
+      },
+    ],
+  },
+  {
+    title: 'House',
+    color: 'bg-yellow-300',
+    tasks: [],
+  },
+];
 
+function App() {
+  const [selectedTaskList, setSelectedTaskList] = useState(null);
   const [emblaRef] = useEmblaCarousel();
+
+  const closeTaskList = () => {
+    setSelectedTaskList(null);
+  };
 
   return (
     <main className="h-screen flex flex-col items-center py-5 bg-[url('src/assets/fondo1.jpg')] bg-cover">
-      <div className="bg-slate-50 w-[500px] h-full p-12 m-4">
+      <div className="bg-slate-50 w-[450px] h-full-screen p-8 m-4">
         <div className="text-center max-w-7xl w-full p-6">
           <h1 className="text-3xl mb-10">
             <span className="font-extrabold">Tasks</span>{' '}
@@ -23,8 +89,44 @@ function App() {
             <span className="font-semibold text-gray-300">Add List</span>
           </div>
         </div>
-        <div>soy un carrusel de notas</div>
+
+        <div className={`flex-1 overflow-hidden`} ref={emblaRef}>
+          <div className={`flex items-end gap-5 p-3 h-72`}>
+            {TASKS_LISTS.map((taskList, i) => {
+              return (
+                <TaskList
+                  key={taskList.title}
+                  title={taskList.title}
+                  color={taskList.color}
+                  tasks={taskList.tasks}
+                  className={`${
+                    selectedTaskList !== null && selectedTaskList !== i
+                      ? '!opacity-0'
+                      : ''
+                  }`}
+                  id={`taskList-${i}`}
+                  handleClick={() => setSelectedTaskList(i)}
+                />
+              );
+            })}
+          </div>
+        </div>
       </div>
+
+      <AnimatePresence>
+        {selectedTaskList !== null && (
+          <motion.div className="fixed w-[40%] h-[95%] z-20 p-2">
+            <TaskList
+              expanded
+              handleClose={closeTaskList}
+              id={`taskList-${selectedTaskList}`}
+              title={TASKS_LISTS[selectedTaskList].title}
+              color={TASKS_LISTS[selectedTaskList].color}
+              tasks={TASKS_LISTS[selectedTaskList].tasks}
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </main>
   );
 }
---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,119 @@
+import Task from '../Task/Task';
+import { motion } from 'framer-motion';
+import { BiX } from 'react-icons/bi';
+
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+      delayChildren: 0.15,
+    },
+  },
+};
+
+const TaskList = ({
+  color,
+  title,
+  tasks = [],
+  handleClick,
+  id,
+  classname,
+  expanded,
+  handleClose,
+}) => {
+  return (
+    <motion.div
+      layout={id}
+      className={`flex-shrink-0 w-[50%] z-50 max-h-full h-[inherit] overflow-y-auto overflow-x-hidden snap-center text-black bg-white border border-gray-600 p-5  rounded-md ${classname} ${
+        expanded ? 'h-full !w-full rounded-none' : ''
+      }`}
+      onClick={handleClick}
+    >
+      <motion.div
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 1 }}
+        className={`h-full flex-col`}
+      >
+        <h2
+          className={`flex justify-between text-xl font-medium pb-4 border-b border-gray-600 mb-4 tracking-tighter ${
+            expanded ? '!text-3xl pb-6 mb-6' : ''
+          }`}
+        >
+          {title}
+
+          {expanded && (
+            <motion.button
+              initial={{
+                opacity: 0,
+                scale: 0,
+              }}
+              animate={{
+                opacity: 1,
+                scale: 1,
+                transition: {
+                  delay: 0.3,
+                },
+              }}
+              className="mr-8"
+              onClick={handleClose}
+            >
+              <BiX size={32} />
+            </motion.button>
+          )}
+        </h2>
+        <motion.ul
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className={`flex-1 flex-shrink-0 overflow-y-auto flex flex-col justify-start scrollbar-hidden ${
+            expanded ? 'gap-6' : 'gap-4'
+          }`}
+        >
+          {tasks.length ? (
+            tasks.map(task => (
+              <Task
+                key={task.title}
+                title={task.title}
+                color={task.color}
+                completed={task.completed}
+                expanded={expanded}
+              />
+            ))
+          ) : (
+            <span className="text-gray-600">No tasks.</span>
+          )}
+        </motion.ul>
+        {expanded && (
+          <motion.div
+            initial={{
+              y: 20,
+              opacity: 0,
+            }}
+            animate={{
+              y: 0,
+              opacity: 1,
+            }}
+            transition={{
+              delay: 0.3,
+              type: 'spring',
+              mass: 0.01,
+              damping: 10,
+            }}
+            className="pr-8"
+          >
+            <input
+              type="text"
+              placeholder="What do you have to do?"
+              className="w-full bg-white mt-6 border border-gray-600 rounded-lg p-4"
+            />
+          </motion.div>
+        )}
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default TaskList;
